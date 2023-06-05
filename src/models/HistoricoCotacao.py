d4ea5b41b2c0558916d61db6f4790f06d3bdd7ad
F2964862 Eric Ribeiro Fernandes <eric.ribeiro@bb.com.br>
2023-06-04 22:55:40 -0300

    adicionando cors aos endpoints

--- src/models/HistoricoCotacao.py
+++ src/models/HistoricoCotacao.py
@@ -1,9 +1,12 @@
 import yfinance as web
 from flask_restful import Resource
+from flask_cors import  cross_origin
+
 
 class HistoricoCotacao(Resource):
-    
+
+    @cross_origin(supports_credentials=True)
     def get(self, ticker):
         _ticker = web.Ticker(ticker + ".SA")
         
-        return _ticker.history(period="3mo").to_json()
+        return _ticker.history(period="3mo").rename(columns = {'Stock Splits': 'StocksSplits'} ).to_json(orient='table')
--- src/models/InfoAtivo.py
+++ src/models/InfoAtivo.py
@@ -1,8 +1,11 @@
 import yfinance as web
 from flask_restful import Resource
+from flask_cors import  cross_origin
+
 
 class InfoAtivo(Resource):
     
+    @cross_origin(supports_credentials=True)
     def get(self, ticker):
         _ticker = web.Ticker(ticker + ".SA")
         return _ticker.info
