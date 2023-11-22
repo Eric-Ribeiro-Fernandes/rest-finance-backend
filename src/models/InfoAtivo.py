cdeae81de893300f8afdd0bbb9ca6f262ca09f30
F2964862 Eric Ribeiro Fernandes <eric.ribeiro@bb.com.br>
2023-11-22 13:37:33 -0300

    Ajustando requisicao de informacoes para fundos imobiliarios

--- src/models/InfoAtivo.py
+++ src/models/InfoAtivo.py
@@ -7,5 +7,20 @@ class InfoAtivo(Resource):
     
     @cross_origin(supports_credentials=True)
     def get(self, ticker):
-        _ticker = web.Ticker(ticker + ".SA")
-        return dict(_ticker.fast_info)
+        _ticker = self.busca_ticker(ticker)
+
+        # resposta = {}
+        # for chave in _ticker.basic_info.keys():
+        #     resposta[chave] = _ticker.basic_info[chave]
+        return _ticker
+
+    def busca_ticker(self, ticker):
+        sa =  web.Ticker(ticker + ".SA").basic_info
+
+        chaves = [chave for chave in sa.keys() if chave != 'marketCap']
+
+        resposta = {}
+        for chave in chaves:
+            resposta[chave] = sa[chave]
+
+        return resposta
